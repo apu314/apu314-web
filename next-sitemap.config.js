f1451677e1ed160d745cd21dc1429a0fda7dccbb
Adolfo Unturbe <apu314@gmail.com>
2023-04-02 03:58:04 +0100

    hardcode siteUrl for next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.VERCEL_URL || 'https://apu314.com',
+  siteUrl: 'https://apu314.com',
   generateRobotsTxt: true,
   generateIndexSitemap: false
 }
