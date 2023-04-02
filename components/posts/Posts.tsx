497c9d0c6bc191703c8d978a3843c61f51481a4e
Adolfo Unturbe <apu314@gmail.com>
2023-04-02 01:49:19 +0100

    add some commented code until final decision

--- components/posts/Posts.tsx
+++ components/posts/Posts.tsx
@@ -8,6 +8,7 @@ interface Props {
 
 const Posts: FC<Props> = ({ posts }) => {
   return (
+    // <div className="grid grid-flow-row gap-3 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
     <div className="grid grid-flow-row gap-3 text-neutral-600 sm:grid-cols-1">
       {posts.map((post: IPost) => (
         <PostCard key={post.slug} post={post} />
