964d838909cc0fc22f7bc1a00c4c08f5e2dc5bca
Adolfo Unturbe <apu314@gmail.com>
2023-04-03 13:39:27 +0100

    update build-algolia-search

---
+++ scripts/build-algolia-search.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const algoliasearch = require('algoliasearch')
+const client = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+  process.env.ALGOLIA_SEARCH_ADMIN_KEY
+)
+
+const index = client.initIndex('BLOG_POSTS')(
+  // console.log('index -->', index)
+
+  async function () {
+    const posts = getAllPublishedPostsAsc('content/posts')
+    console.log(posts)
+    await index
+      .saveObjects(posts, {
+        autoGenerateObjectIDIfNotExist: true
+      })
+      .then(({ objectIDs }) => {
+        console.log(objectIDs)
+      })
+      .catch(console.error)
+  }
+)()
