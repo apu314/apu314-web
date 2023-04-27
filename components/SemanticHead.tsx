daaa5d426e26c12ed1d1a5852a5f498ab5aaf224
Adolfo Unturbe <apu314@gmail.com>
2023-04-27 18:39:01 +0100

    add Twitter Cards

--- components/SemanticHead.tsx
+++ components/SemanticHead.tsx
@@ -24,10 +24,21 @@ const SemanticHead: FC<Props> = (props) => {
       <meta property="og:description" content={description} />
       {image && <meta property="og:image" content={image} />}
 
-      <link
+      {/* Twitter Cards */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@adolfo_unturbe" />
+      <meta name="twitter:creator" content="@adolfo_unturbe" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {image && <meta name="twitter:image" content={image} />}
+      {image && <meta name="twitter:image:alt" content={`${title} image`} />}
+
+      {/* TODO: LOGO FAVICON */}
+      {/* <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💻</text></svg>"
-      />
+      /> */}
+      <link rel="icon" href="/favicon.ico" />
 
       {jsonLD && (
         <script
