322a23431eb7c457c5d66e27e32e699932cafdca
Adolfo Unturbe <apu314@gmail.com>
2023-04-27 19:17:10 +0100

    add footer with social links

--- layouts/MainLayout/Footer.tsx
+++ layouts/MainLayout/Footer.tsx
@@ -3,7 +3,48 @@ import { FC } from 'react'
 type Props = {}
 
 const Footer: FC<Props> = (props) => {
-  return <div className="hidden">Footer</div>
+  return (
+    <footer className="footer">
+      <ul>
+        <li>
+          <a
+            href="https://github.com/apu314"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://linkedin.com/in/adolfounturbe/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/adolfo_unturbe"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://instagram.com/apu314"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+        </li>
+      </ul>
+    </footer>
+  )
 }
 
 export default Footer
